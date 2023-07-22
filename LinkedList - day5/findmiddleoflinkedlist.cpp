b9878af8f919374b39682880eca484beb98869c8
prophecyprogrammer <vatsal.shah2304@gmail.com>
2023-07-23 00:41:23 +0530

    Find middle element

---
+++ LinkedList - day5/findmiddleoflinkedlist.cpp
@@ -0,0 +1,33 @@
+// Brute Force solution
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+        
+//         int ct = 0;
+//         ListNode* temp = head;
+//         while(temp!=nullptr){
+//             ct++;
+//             temp=temp->next;
+//         }
+//         int to_find = ct/2+1;
+//         int pos = 1;
+//         ListNode* ans = head;
+//         while(pos<to_find){
+//             ans = ans->next; 
+//             pos++;
+//         }
+//         return ans;
+
+//     }
+// };
+//tc = > o(n)+O(n/2)
